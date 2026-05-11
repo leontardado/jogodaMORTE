99c20fb96e9a488a7150b44d676ad1a3cac70008
aluno <joao.neves@alunos.ifsuldeminas.edu.br>
2026-05-11 11:30:07 -0300

    11/05/2026

--- app/src/main/java/com/example/ohmygod/TelaJogo.java
+++ app/src/main/java/com/example/ohmygod/TelaJogo.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,18 +13,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class TelaJogo extends AppCompatActivity implements View.OnClickListener {
+public class TelaJogo extends AppCompatActivity implements View.OnClickListener{
+
     private ImageView imagem;
-    private ArrayList<Integer> listaImagens;
-    private int indicelistaImagens;
+    private ArrayList<Integer> listaImagens, listaIdsButtons;
+    private int indiceListaImagens;
+    private ArrayList<String> listaPalavras;
+    private Button b1;
+    private TextView texto;
+    private String palavra;
+    private char[] estado;
 
-   private Button b1;
-
-    @Override
-    public void onClick(View v) {
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,33 +33,132 @@ public class TelaJogo extends AppCompatActivity implements View.OnClickListener 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_jogo);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                    return insets;
-                });
-            imagem = findViewById(R.id.imageView2);
-            indicelistaImagens =0;
-            listaImagens = new ArrayList<Integer>();
-            listaImagens.add(R.drawable.forca_1_9);
-            listaImagens.add(R.drawable.forca_2_9);
-            listaImagens.add(R.drawable.forca_3_9);
-            listaImagens.add(R.drawable.forca_4_9);
-            listaImagens.add(R.drawable.forca_5_9);
-            listaImagens.add(R.drawable.forca_6_9);
-            listaImagens.add(R.drawable.forca_7_9);
-            listaImagens.add(R.drawable.forca_8_9);
-            listaImagens.add(R.drawable.forca_9_9);
-            listaImagens.add(R.drawable.forca_10_9);
-            listaImagens.add(R.drawable.forca_11_9);
-            b1 = findViewById(R.id.id1);
-            b1.setOnClickListener(this);
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        imagem = findViewById(R.id.imageView2);
+        indiceListaImagens = -1;
+        listaImagens = new ArrayList<Integer>();
+        listaImagens.add(R.drawable.forca_1_9);
+        listaImagens.add(R.drawable.forca_2_9);
+        listaImagens.add(R.drawable.forca_3_9);
+        listaImagens.add(R.drawable.forca_4_9);
+        listaImagens.add(R.drawable.forca_5_9);
+        listaImagens.add(R.drawable.forca_6_9);
+        listaImagens.add(R.drawable.forca_7_9);
+        listaImagens.add(R.drawable.forca_9_9);
+        listaImagens.add(R.drawable.forca_10_9);
+        listaImagens.add(R.drawable.forca_11_9);
 
-        }
-        public void atualizaForca(){
-            indicelistaImagens++;
-            imagem.setImageResource(listaImagens.get(indicelistaImagens));
+        listaPalavras = new ArrayList<String>();
 
+        listaPalavras.add("HUMANO");
+        listaPalavras.add("FELICIDADE");
+        listaPalavras.add("INUTIL");
+        listaPalavras.add("PALAVRA");
+        listaPalavras.add("MACACO");
+        listaPalavras.add("BASQUETE");
+        listaPalavras.add("ESPORTE");
+        listaPalavras.add("INDIAN");
+        listaPalavras.add("NETANYAHU");
+
+
+
+        texto = findViewById(R.id.textView3);
+
+        listaIdsButtons = new ArrayList<Integer>();
+        listaIdsButtons.add(R.id.id1);
+        listaIdsButtons.add(R.id.id2);
+        listaIdsButtons.add(R.id.id3);
+        listaIdsButtons.add(R.id.id4);
+        listaIdsButtons.add(R.id.id5);
+        listaIdsButtons.add(R.id.id6);
+        listaIdsButtons.add(R.id.id7);
+        listaIdsButtons.add(R.id.id8);
+        listaIdsButtons.add(R.id.id9);
+        listaIdsButtons.add(R.id.id10);
+        listaIdsButtons.add(R.id.id11);
+        listaIdsButtons.add(R.id.id12);
+        listaIdsButtons.add(R.id.id13);
+        listaIdsButtons.add(R.id.id14);
+        listaIdsButtons.add(R.id.id15);
+        listaIdsButtons.add(R.id.id16);
+        listaIdsButtons.add(R.id.id17);
+        listaIdsButtons.add(R.id.id18);
+        listaIdsButtons.add(R.id.id19);
+        listaIdsButtons.add(R.id.id20);
+        listaIdsButtons.add(R.id.id21);
+        listaIdsButtons.add(R.id.id22);
+        listaIdsButtons.add(R.id.id23);
+        listaIdsButtons.add(R.id.id24);
+        listaIdsButtons.add(R.id.id25);
+        listaIdsButtons.add(R.id.id26);
+
+        for (int j = 0; j<listaIdsButtons.size();j++){
+            Button b = findViewById(listaIdsButtons.get(j));
+            b.setOnClickListener(this);
         }
+        inicializaJogo();
+
+    }
+
+    public void inicializaJogo(){
+        imagem.setImageResource(R.drawable.forca_0_9);
+        indiceListaImagens=0;
+        palavra = sorteiaPalavra();
+        estado = new char[palavra.length()];
+        for(int i =0; i<estado.length;i++){
+            estado[i] = '_';
+        }
+        atualizaTexto();
 
 
     }
+public void verificaLetra(char c){
+        boolean status = false;
+        for(int i=0; i<palavra.length();i++){
+            if(palavra.charAt(i)==c){
+                status = true;
+                        estado[i] = c;
+
+            }
+        }
+        if(!status){
+           atualizaForca();
+        }
+        else{
+            atualizaTexto();
+        }
+
+}
+    public void atualizaTexto() {
+        String temporaria = new String();
+        temporaria = "";
+        for (int i = 0; i < estado.length; i++) {
+            temporaria += estado[i] + "";
+        }
+        texto.setText(temporaria);
+
+    }
+
+    public String sorteiaPalavra(){
+        String retorno = new String();
+        Collections.shuffle(listaPalavras);
+        retorno = listaPalavras.get(0);
+        return retorno;
+    }
+    public void atualizaForca(){
+        indiceListaImagens++;
+        imagem.setImageResource(listaImagens.get(indiceListaImagens));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button b = (Button) v;
+        verificaLetra(b.getText().toString().charAt(0));
+        b.setEnabled(false);
+
+
+    }
+}
